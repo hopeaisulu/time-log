dda4adc40a49ada6fd82e285f25bf58b90c261e1
Aisulu Abdraimova <aisulu@inkubasia.com>
2024-07-01 01:19:09 +0600

    List fake time logs from frontend
    Summary:
    * Create a component to show list of logged Logged times
     * Use hooks like `useState();` to initialize state of the list and `useEffect();` to retrieve data
    * Create an interface to describe what is a TimeLog
    * Use newly created component in `App.tsx`
    
    Test plan:
    * Loaded `http://127.0.0.1:8000/` and saw list of logged times and a title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import Button from '@mui/material/Button';
 import './App.css';
+import LoggedTimesList from './components/time-log/LoggedTimesList';
 
 function App() {
   return(
   <div className="App">
   <Button variant="outlined">Hello world</Button>
+  <LoggedTimesList />
 </div>)
 }
 
